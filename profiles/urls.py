1b142dfe963615e7c1719c8edbc0747b941e4866
Emanuel Sandoval <davidemanuelsandoval@gmail.com>
2018-12-05 10:39:58 -0300

    added users_lst template

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,5 +3,6 @@ import views
 
 
 urlpatterns = [
-    url(r'^$', views.users)
+    url(r'^$', views.users),
+    url(r'users_lst/', views.UsersView.as_view(), name='simple_lst'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
 from .models import Users
@@ -12,3 +13,12 @@ def users(request):
     """
     users_lst = Users.objects.all()
     return render(request, 'index.html', {'users_lst': users_lst})
+
+
+class UsersView(TemplateView):
+    template_name = 'users_lst.html'
+
+    def get_context_data(self, **kwargs):
+        users = Users.objects.all()
+        context = {'users': users}
+        return context
