d35cfec82289807c8bc6669a9c263d7317b9c5f3
Emanuel Sandoval <davidemanuelsandoval@gmail.com>
2018-12-05 11:04:35 -0300

    backup

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import TemplateView, DetailView
 
 # Create your views here.
 from .models import Users
@@ -16,9 +16,25 @@ def users(request):
 
 
 class UsersView(TemplateView):
+    """
+    Shows al the users and their addresses.
+    """
     template_name = 'users_lst.html'
 
     def get_context_data(self, **kwargs):
         users = Users.objects.all()
         context = {'users': users}
         return context
+
+
+class UserDetailView(DetailView):
+    """
+    Show the user details.
+    """
+    model = Users
+
+    def get_context_data(self, request, pk):
+        user = get_object_or_404(Users, pk)
+        context = {'user': user}
+        if user:
+            return render(request, 'details.html', context)
