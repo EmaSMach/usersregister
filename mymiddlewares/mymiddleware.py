261a1f870634c7b987c2593342a58ef64b88712d
Emanuel Sandoval <davidemanuelsandoval@gmail.com>
2018-12-07 01:19:39 -0300

    corrected error at returned render_to_response, direct context dict passed

---
+++ mymiddlewares/mymiddleware.py
@@ -0,0 +1,58 @@
+# A simple middleware to check if the user email is an @gmail one.
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user
+from django.shortcuts import redirect
+from django.core.validators import ValidationError
+import re
+
+
+def is_gmail_email(email):
+    """
+    Check if the email is a gmail one.
+    """
+    pattern = re.compile(r"\b[\w.%+-]+@gmail.com\b")
+    if not re.match(pattern, email):
+        raise ValidationError("The email is not valid")
+
+
+class MyMiddleware(object):
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # One-time configuration and initialization.
+
+    def __call__(self, request):
+        # Code to be executed for each request before
+        # the view (and later middleware) are called.
+        usera = 'auth.User'
+        #print "TESTEO", request.user, dir(request.user)
+        #print "TESTEO", request.user.get_email_field()
+        response = self.get_response(request)
+        if request.user.is_authenticated():
+            print "Email", request.user.email
+            try:
+                print is_gmail_email(request.user.email)
+            except ValidationError:
+                redirect('http://www.google.com')
+            except TypeError:
+                print "Ponga un usuario."
+
+
+            # user = get_user(request)
+            # print dir(user), "USUARIO", type(user), user.is_authenticated(), user.email
+            # if request.method == "GET":
+            #     if request.GET.get('email'):
+            #         try:
+            #             is_gmail_email(request.GET.get('email'))
+            #         except ValidationError:
+            #             redirect('http://www.google.com')
+            #     else:
+            #         if request.user.is_authenticated():
+            #             try:
+            #                 is_gmail_email(request.user.get('email'))
+            #             except ValidationError:
+            #                 redirect('http://www.google.com')
+
+        # Code to be executed for each request/response after
+        # the view is called.
+
+        return response
--- registration/settings.py
+++ registration/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'mymiddlewares.mymiddleware.MyMiddleware',
 ]
 
 ROOT_URLCONF = 'registration.urls'
