14a4d8d74619276c0ebf69ec5c95e210fba4bd19
David Emanuel Sandoval <35826098+EmaSMach@users.noreply.github.com>
2018-12-08 15:13:00 -0300

    changed DEBUG to False

--- registration/settings.py
+++ registration/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'or&sv0c8myar7ic4yw2d%(-m7cydf9d1rl3e7gxx^ouhj68x8u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
